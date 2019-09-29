c20995a022d463208356b95781b47dd0c13cc570
kuromoka <kuromokapb@gmail.com>
2019-09-29 17:53:42 +0900

    add build list

--- src/quickPick.ts
+++ src/quickPick.ts
@@ -2,33 +2,52 @@ import * as vscode from 'vscode';
 import { Client, BuildNode } from './Client';
 
 export class QuickPick {
-  static readonly LATEST_RETRY_ITEM_LABEL: string = 'Restart Latest Build';
-  static readonly LATEST_BUILD_URL_ITEM_LABEL: string = 'Latest Build Url';
+  static readonly LATEST_RETRY_ITEM_LABEL: string = 'Retry latest build';
+  static readonly LATEST_BUILD_URL_ITEM_LABEL: string = 'Open latest build url';
+  static readonly SHOW_BUILD_LIST_ITEM_LABEL: string = 'Show build list';
 
   private recentBuilds: BuildNode[];
   private latestRetryItem: vscode.QuickPickItem;
   private latestBuildUrlItem: vscode.QuickPickItem;
+  private showBuildListItem: vscode.QuickPickItem;
 
   constructor() {
     this.recentBuilds = [];
     this.latestRetryItem = {
-      label: QuickPick.LATEST_RETRY_ITEM_LABEL
+      label: QuickPick.LATEST_RETRY_ITEM_LABEL,
+      detail: 'Retry latest build'
     };
     this.latestBuildUrlItem = {
-      label: QuickPick.LATEST_BUILD_URL_ITEM_LABEL
+      label: QuickPick.LATEST_BUILD_URL_ITEM_LABEL,
+      detail: 'Open latest build url'
+    };
+    this.showBuildListItem = {
+      label: QuickPick.SHOW_BUILD_LIST_ITEM_LABEL,
+      detail: 'Show build list'
     };
   }
 
   public async showItem(client: Client) { 
-    await vscode.window.showQuickPick([this.latestRetryItem, this.latestBuildUrlItem]).then(selectedItem => {
+    await vscode.window.showQuickPick([this.latestRetryItem, this.latestBuildUrlItem, this.showBuildListItem]).then(selectedItem => {
       switch (selectedItem!.label) {
         case QuickPick.LATEST_RETRY_ITEM_LABEL:
-            client.retryBuild(this.recentBuilds[0].buildNum);
-            client.updateBuildStatus();
+            client.retryBuild(this.recentBuilds[0].buildNum).then(() => {
+              client.updateBuildStatus();
+            });
             break;
         case QuickPick.LATEST_BUILD_URL_ITEM_LABEL:
           vscode.env.openExternal(vscode.Uri.parse(this.recentBuilds[0].buildUrl));
           break;
+        case QuickPick.SHOW_BUILD_LIST_ITEM_LABEL:
+            let items: vscode.QuickPickItem[] = [];
+            this.recentBuilds.forEach((recentBuild: BuildNode) => {
+              items.push({
+                label: recentBuild.status + ': ' + recentBuild.branch + ' #' + recentBuild.buildNum,
+                detail: recentBuild.subject
+              });
+            });
+            vscode.window.showQuickPick(items);
+            break;
         default:
           break;
       }
@@ -37,7 +56,5 @@ export class QuickPick {
 
   public updateItem(recentBuilds: BuildNode[]) {
     this.recentBuilds = recentBuilds;
-    this.latestRetryItem.detail = 'Retry latest nuild';
-    this.latestBuildUrlItem.detail = 'Open latest build url';
   }
 }
