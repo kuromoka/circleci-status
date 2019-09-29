7831e480a51895f78c6ed97e1a2747e06bd8bfc9
kuromoka <kuromokapb@gmail.com>
2019-09-29 10:46:22 +0900

    get recent builds

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,32 +1,70 @@
 import * as vscode from 'vscode';
 import axios, { AxiosResponse } from 'axios';
 
+export interface BuildNode {
+	status: string;
+	buildUrl: string;
+	buildNum: Int16Array;
+	subject: string;
+	branch: string;
+	committerName: string;
+}
+
 export class ApiClient {
 	static readonly API_ENTRY_POINT: string = 'https://circleci.com/api/v1.1';
 
 	private apiToken: string | undefined;
-	private projectName: string | undefined;
+	private vcsType: string;
 	private userName: string | undefined;
+	private projectName: string | undefined;
 
 	constructor() {
 		this.apiToken = vscode.workspace.getConfiguration('circleciStatus').get('apiToken');
 		vscode.workspace.onDidChangeConfiguration(() => {
 			this.apiToken = vscode.workspace.getConfiguration('circleciStatus').get('apiToken');
 		});
+		// feature: selectable github/bitbucket
+		this.vcsType = 'github';
 		this.projectName = vscode.workspace.name;
+	}
 
+	public setUserName() : Promise<any> {
 		// set username from '/me' result
-		this.requestApi('me')
-			.then(data => this.userName = data.name)
+		return this.requestApi('me')
+			.then((response : any) => {
+				this.userName = response.data.name;
+				return this.userName;
+			})
+			.catch((err : any) => console.error(err));
+	}
+
+	public getRecentBuilds() : Promise<any> {
+		let recentBuils: BuildNode[] = [];
+		const path : string = 'project/' + this.vcsType + '/' + this.userName + '/' + this.projectName;
+
+		return this.requestApi(path)
+			.then(response => {
+				response.data.forEach((element: any) => {
+					recentBuils.push({
+						status: element.status,
+						buildUrl: element.build_url,
+						buildNum: element.build_num,
+						subject: element.subject,
+						branch: element.branch,
+						committerName: element.committer_name,
+					});
+				});
+				return recentBuils;
+			})
 			.catch(err => console.error(err));
 	}
 
 	private async requestApi(path : string) : Promise<any> {
 		try {
-			const result  = await axios.get(ApiClient.API_ENTRY_POINT + '/' + path + '?circle-token=' + this.apiToken);
-			return result.data;
+			const response = await axios.get(ApiClient.API_ENTRY_POINT + '/' + path + '?circle-token=' + this.apiToken);
+			return response;
 		} catch (err) {
-			throw new err;
+			throw new Error(err);
 		}
 	}
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,19 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { ApiClient } from './apiClient';
+import { ApiClient, BuildNode } from './apiClient';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const apiClient = new ApiClient();
+	apiClient.setUserName()
+		.then(data1 => {
+			return apiClient.getRecentBuilds();
+		})
+		.then(data2 => {
+			console.log(data2);
+		});
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
